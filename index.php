4fd5c4e7a5d3ce2c287ca8fcea2ca8fab5f8257a
fainla <ainlapoiss@hot.ee>
2014-10-06 12:33:08 +0300

    final 6.10

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
 <meta http-equiv="Content type" content="text/html; charset=utf-8">
 </head>
 <body>
-<?php echo "Tere!" ?>
+<?php echo "Esimene leht" ?><br>
+<?php $link_file="second_page.php";
+$age =32;
+ ?>
+<a href="second_page.php?age=32">Second page</a>
+
+<?php
+	print_r($_GET);
+?>
+
+<?php 
+	$jada = array('frank', '24');
+	echo $jada[1];
+?>
+
+
+<?php echo "<h1>" . $_GET["name"] . "</h1>"; ?>  
+
+<?php  echo "Minu nimi on " . $_GET["name"] . $_GET["age"] ?>
 </body>
 </html>
--- second_page.php
+++ second_page.php
@@ -4,6 +4,11 @@
 <meta http-equiv="Content type" content="text/html; charset=utf-8">
 </head>
 <body>
-<?php echo "Tere!" ?>
+<?php echo "Teine leht" ?><br>
+<a href="index.php?name=knarf&age=32">First page</a>
+<?php
+print_r($_GET);
+?>
+
 </body>
 </html>
